e7f6603af3a577ac82bfb08c573c9014783768d7
Lakshman Guptha <lakshman542@gmail.com>
2022-04-14 18:42:48 +0530

    added and deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,5 +4,8 @@ public class LoginPage {
 
 	public static void main(String a[]) {
 		System.out.println("Hello LP");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("logged in..");
 	}
 }
